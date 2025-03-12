96ec8bb4db5c9cf57fe80555c3975a6edaec02c1
sisyphys_666 <139012668+Umer-Islam@users.noreply.github.com>
2025-03-12 08:02:44 +0500

    remove comments

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-// let url = "https://www.quoterism.com/api/quotes/random";
-// let url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
-// let url = "https://type.fit/api/quotes";
 let url = "https://dummyjson.com/quotes/random";
-// let url = "https://catfact.ninja/fact";
 
 fetch(url)
   .then((res) => res.json())
